f0aad10b5760f96934de103d8a690eb3fc77e3d4
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-01-23 22:50:12 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -10,11 +10,11 @@ for i in range(t):
   # for i in range(n):
     # if word[i] != "A" or word[i] != "a"  && word[i] != "E" or word[i] != "e" .....till "u"and "U"         #Bruteforce method
      
-    # OR covert string to upper or lower case using .upper() and .lower() then check for just lowercase , 5 vowels arre only needed 
+    # OR covert string to upper or lower case using .upper() and .lower() then check for just lowercase , 5 vowels are only needed   but we should not directly manipulate input rather take a dublicate variable
     for char in word:
         if char not in 'aeiou':
             count += 1             # count++ is not a valid expression in Python
-            if count >=4 :      # IMP
+            if count >=4 :      # IMP   if we write nested if
                 break         #we are checking the adjacent characters , so breaking out of loop once 4 consonants are acquired
         else:
             count = 0     #note here the count resets once encountered a vowel , it may have had reset count even after 5 consonant is enocountered FOLLOWED BY A VOWEL if not for the above condition
@@ -24,3 +24,29 @@ for i in range(t):
     else:
         print("YES")
     
+
+t = int(input())
+for i in range(t):
+    
+    ddsa, dtoc ,ddm = map(int , input().split())    #since single line input
+    sdsa, stoc , sdm = map(int , input().split())   
+    dtot = ddsa + dtoc + ddm
+    stot = sdsa + stoc + sdm
+    
+    if stot == dtot:
+        if sdsa == ddsa:
+            if dtoc == stoc:
+                print("TIE")
+            elif dtoc > stoc:
+                    print("DRAGON")
+            else :
+                print("SLOTH")
+        elif ddsa > sdsa:
+            print("DRAGON")
+        else :
+            print("SLOTH")
+    elif dtot > stot :
+        print("DRAGON")
+    else :
+        print("SLOTH")
+        

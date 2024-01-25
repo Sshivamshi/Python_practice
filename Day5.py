9b8d72549f9dcb4634b155f995f1055615503785
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-01-25 21:48:16 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,26 @@
+my_message = "himmi"
+message = """ This
+              is a multiline string  """ 
+print(my_message , message)              
+print(my_message)  
+print(message)  
+''' 
+OUTPUT: 
+himmi  This      (Output on same line)
+              is a multiline string 
+
+himmi        
+ This              (Second line of output)
+              is a multiline string  
+'''
+
+
+'''
+This is a multi-line comment.
+It spans multiple lines.
+'''
+
+"""
+Another way to create a multi-line comment.
+Use triple double quotes .
+"""

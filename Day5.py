207cc36c3e5f3cd6cec9a983b944cfefdedce6df
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-01-25 22:56:33 +0530

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -24,3 +24,30 @@ It spans multiple lines.
 Another way to create a multi-line comment.
 Use triple double quotes .
 """
+
+
+message = "Bruce is goodg"
+print(len(message)) #13 (includes space )
+print(message[0:5])  #Bruce   (prints range of characters from a string )
+
+# STRING METHODS 
+
+print(message.lower())   # bruce is goodg
+#Takes string input and return lowercase of that string 
+
+print(message.upper())  # BRUCE IS GOODG
+#Takes string input and return uppercase of that string 
+
+print(message.count("o")) # 2  
+
+#takes char,string and returns number of times it appeared
+print(message.count("good")) # 1
+
+#takes string and returns index of first letter of word/string
+print(message.find("good")) # 9 
+
+#Takes a char/Alphabet and returns the index of FIRST OCCURENCE"
+print(message.find("g")) # 9 
+
+message = message.replace( 'good', 'bad' ) # Bruce is badg  
+print(message)

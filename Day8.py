bfcfb6820594b14d7a0a66fc185edebed59112c5
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-01-29 10:17:06 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -19,3 +19,18 @@ def factorial(N):
     print(fact)
     
 factorial(4)
+
+
+# FACTORIAL USING FOR LOOP and counting Digits in factorial 
+
+def factorial(N):
+    fact = 1
+    for i in range(1,N+1):    #as required range is 1 to N  [ range(5) means 0-4
+        fact *= i
+    
+    print(fact)
+    s = str(fact)      # converting int to string(so that it becomes iterable , can be later turned to list and find length / no. of element)
+    list1 = list(s)
+    print(len(s))       #print number of digits in the factorial 
+    
+factorial(4)

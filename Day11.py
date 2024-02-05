137c9f188224f4882c2a50a1bdf0e432d667a2cf
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-02-05 21:56:02 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -63,3 +63,47 @@
 # Syntax: string.join(iterable)
 # Argument Type: iterable
 # Return Type: str
+
+
+message = '"hello"'
+print(message)     # OUTPUT : "hello"   :use of single quote to print ""
+
+# string slicing operation 
+print(message[0])   
+print(message[-1])
+# syntax : message[from index : upto position]
+print(message[1:6]) 
+print(message[1:2])
+
+# string methods : .methods()
+
+#.upper() : Returns the uppercased version of string 
+print(message.upper())
+
+#.find('substring') /   : Takes substring as argument and returns the index of the first occurrence of the searched substring and -1 if searched substring isn't found
+string = 'word Find the word from here'
+print(string.find('word'))
+print(string.find('word2'))
+print(string.find('word' ,1)) 
+print(string.find('word' ,1 , 15)) 
+
+# string.replace(old, new, count)  : Returns a copy of the string where occurrences of a substring are replaced with another substring. 
+string2 = 'word Find the word from here'
+string2.replace('word' , 'word2')
+print(string2)  # Returns same string (string2) :  as .replace() method returns a copy of modified string IT DOESN'T modify the original string
+# print(string.replace('WORD' , 'replaced'))   # .replace()'s  arguments are case sensitive
+print(string.replace('word' , 'replaced'))
+print(string.replace('word' , 'replaced' , 1)) # count (optional): The maximum number of occurrences to replace. If not specified, all occurrences will be replaced.
+
+
+# python fstring ( replacement of .format method)
+lists = ['Himmi' , 'Akii' , 'Bruce' , 'Mohit' ]
+for i in range(len(lists)):
+    print(f"Hello {lists[i]} , u good ?")
+'''
+OUTPUT:
+Hello Himmi , u good ?
+Hello Akii , u good ?
+Hello Bruce , u good ?
+Hello Mohit , u good ?
+'''

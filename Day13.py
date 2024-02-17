e3b2487b4b97506a8e78276aa68dc4b39b58c452
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-02-18 00:15:58 +0530

    Update Day13.py

--- Day13.py
+++ Day13.py
@@ -9,6 +9,11 @@ for i in range(t):
     else :
         print(A)
 
+t = int(input())
+for i in range(t):
+    N , A = map(int , input().split())
+    print(min((A), (N-A)))
+
 # https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/HEADBOB
 
 

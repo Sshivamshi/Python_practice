f5c247daf0ad7a88f626df17eabf66522a7b94f8
Sshivamshi <118373241+Sshivamshi@users.noreply.github.com>
2024-02-17 22:55:46 +0530

    Update Day13.py

--- Day13.py
+++ Day13.py
@@ -1,2 +1,10 @@
 # https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/MASKPOL
 
+# cook your dish here
+t = int(input())
+for i in range(t):
+    N , A = map(int, input().split())
+    if (A) >= (N + 1)/2 :
+        print(N-A)
+    else :
+        print(A)
